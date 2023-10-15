251077d3455d92b3e9c88933bbb70a39c4913496
JoaoRibeironetogit config --global user.email joaoribeirocoelhoneto@gmail.comgit config user.namegit config user.emailgit config user.namegit config init.defaultbranchgit config --global user.name JoaoRibeironeto <joaoribeirocoelhoneto@gmail.com>
2023-10-15 11:21:05 -0300

    Meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      //TODO:conhecer e importar a classe scanner
+      //exibir as mensagens para o usuario
+      //obter pela scanner os valores digitados no terminal
+      //exibir a mensagem conta criada
+
+      
+
+
     }
 }
