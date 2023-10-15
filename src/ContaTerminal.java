76e39178263fee30945fb60f8f6cfe804f4c5c41
JoaoRibeironetogit config --global user.email joaoribeirocoelhoneto@gmail.comgit config user.namegit config user.emailgit config user.namegit config init.defaultbranchgit config --global user.name JoaoRibeironeto <joaoribeirocoelhoneto@gmail.com>
2023-10-15 13:07:19 -0300

    atualizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,11 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-      //TODO:conhecer e importar a classe scanner
-      //exibir as mensagens para o usuario
-      //obter pela scanner os valores digitados no terminal
-      //exibir a mensagem conta criada
+    
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-      
+    System.out.println(" Digite seu nome: ");
+    String nome = scanner.next();
+
+    System.out.println(" Digite o número da agência: ");
+    String agencia = scanner.next();
+
+    System.out.println(" Digite o número da conta: ");
+    int conta = scanner.nextInt();
+
+    System.out.println(" Digite o saldo da conta: ");
+    double saldo = scanner.nextDouble();
+
+    System.out.println("Olá" + nome +","+"obrigado por criar uma conta em nosso banco, sua agencia é "+agencia+"," + "conta" + conta + "e seu saldo"+saldo+"ja esta disponível para saque.");
+
+    
+    
+    
+    
+    
+       
+
+
 
 
     }
